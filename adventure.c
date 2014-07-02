92bae1f8f725660a2afe33fb7d4ba365e4b33198
Joshua O'Leary <joshua.oleary@btinternet.com>
2014-07-02 18:50:38 +0100

    Added option to specify data path

--- adventure.c
+++ adventure.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "adventure.h"
 #include "data.h"
 #include "player.h"
 
-void parse_args(int argc, char **argv)
+#define PATH_BUF_SIZE 100
+
+void parse_args(int argc, char **argv, char *dpathbuf)
 {
 	debug = 0;
 	
@@ -16,11 +19,28 @@ void parse_args(int argc, char **argv)
 			printf("Enabling debug mode\n");
 			debug = 1;
 		}
+		else if( COMP_STR(argv[i], "--data-path" ) ) {
+			if( argc > (i+1) ) {
+				printf("Setting data path to %s\n", argv[i+1]);
+				strncpy(dpathbuf, argv[i+1], PATH_BUF_SIZE);
+			}
+			else {
+				printf("Error: path location invalid\n");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
 }
 int main(int argc, char **argv)
 {
-	parse_args(argc, argv);
+	char datapath_buffer[PATH_BUF_SIZE] = { 0 };
+	parse_args(argc, argv, datapath_buffer);
+	if( strlen(datapath_buffer) > 0 ) {
+		if( chdir(datapath_buffer) == -1 ) {
+			printf("Error: data path does not exist\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 	
 	roomBase rooms;
 	objectBase objects;
